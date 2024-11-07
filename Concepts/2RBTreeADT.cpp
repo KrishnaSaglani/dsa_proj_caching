83539c1bf8ab1a5c8c0e079c276aed25280349c7
Krishna Saglani <b23cs1027@iitj.ac.in>
2024-11-07 17:46:18 +0530

    debugging work on insertion function

--- Concepts/2RBTreeADT.cpp
+++ Concepts/2RBTreeADT.cpp
@@ -3,14 +3,7 @@ using namespace std;
 
 //This shall contain rotation logic as well...so AVL is incorporated into same code
 
-
-
-
-class RBTree
-{
-    private:
-        //node stuct:
-        typedef struct RBNode
+typedef struct RBNode
         {
             int key;
             int value; //dont forget this is actually for an LRU cache
@@ -34,20 +27,56 @@ class RBTree
             }
         }RBNode;
 
-        RBNode * root;
+
+class RBTree
+{
+    private:
+        //node stuct:
+        // typedef struct RBNode
+        // {
+        //     int key;
+        //     int value; //dont forget this is actually for an LRU cache
+        //     int time_stamp;
+        //     RBNode* left;
+        //     RBNode* right;
+            
+        //     RBNode* parent; //new pointer for us
+        //     char colour; //'r' or 'b'
+            
+
+        //     RBNode(int k, int v, int ts)
+        //     {
+        //         key = k;
+        //         value = v;
+        //         left = NULL; right = NULL;
+        //         parent = NULL;
+        //         colour = 'r';
+        //         time_stamp = ts;
+                
+        //     }
+        // }RBNode;
+
+        
         void leftRotate(RBNode * Imb_node)  //Imb_node = Imbalanced node
         {
             RBNode * child = Imb_node->right;
             RBNode * temp = child->left;
             RBNode * source = Imb_node->parent;
-            
 
             //now lets rotate
             child->left = Imb_node;
-            Imb_node->right = temp;
-            temp->parent = Imb_node;
+            Imb_node->right = temp; // or temp
+            if(temp!=NULL)temp->parent = Imb_node;
             Imb_node->parent = child;
             child->parent = source;
+
+            // //extra precaution due to some errors
+            // if(temp == NULL) Imb_node->right = NULL; useless
+
+            //test prints
+            if(Imb_node->right == NULL)cout<<"Imb_node linked well with temp"<<endl;
+            if(child ->left == Imb_node)cout<<endl<<" new root and Imb linked well too"<<endl;
+            if(Imb_node->left == NULL)cout<<"Imb left has stayed NULL";
 
             //relink to source now
             if(source==NULL) return;
@@ -62,6 +91,8 @@ class RBTree
                     source->right = child;
                 }
             }
+
+
            
         }
         //not designed to handle the colors right away
@@ -77,7 +108,7 @@ class RBTree
             //now lets rotate
             child->right = Imb_node;
             Imb_node->left = temp;
-            temp->parent = Imb_node;
+            if(temp != NULL)temp->parent = Imb_node;
             Imb_node->parent = child;
             child->parent = source;
 
@@ -108,24 +139,22 @@ class RBTree
         //btw parent always exists...uncle and grandparent may not
         void Balance(RBNode * child, RBNode* parent)
         {
+            if(parent == NULL) return; // view uncle-'red' case
             if(parent->colour == 'b') return; //yay no problem here
 
             //now else case: parent_>colour == 'r'
             //hence grandparent MUST be black for sure
             RBNode * grandparent = parent->parent;
-            if(grandparent == NULL)
-            {
-                parent->colour = 'b';
-                return;
-            }
+            // if you reach here...grandparent MUST exist for sure
 
             RBNode * uncle = find_sibling(parent);
+            if(uncle==NULL)cout<<"Uncle is NULL"<<endl;
             if(uncle==NULL  || uncle->colour == 'b') //black uncle..in RB trees we consider NULL as black also
             {
                 //rotation then recolouring
                 //assuming grandparent exists..if not then still okay
                 //as it will just be a NULL return point
-                bool left_child = (child->time_stamp<=parent->time_stamp)? true:false;
+                bool left_child = (child->time_stamp <= parent->time_stamp)? true:false;
 
 
                 if(parent->time_stamp <= grandparent->time_stamp) //parent at left
@@ -171,6 +200,8 @@ class RBTree
                 grandparent->colour = 'r';
                 Balance(grandparent,grandparent->parent);
             }
+
+            return;
         }
 
         //height() and balance_check() is not required thanks to RB tree properties
@@ -195,7 +226,7 @@ class RBTree
                 
                 //now current point becomes parent
             }
-            else if(new_node->time_stamp > current->time_stamp)
+            else //if(new_node->time_stamp > current->time_stamp)
             {
                 if(current->right == NULL)
                 {
@@ -204,7 +235,7 @@ class RBTree
                 }
                 else
                 {
-                    current->right->parent = current;
+                    current->right->parent = current;//extra precaution
                     //hence as we go along, the parents are being established also 
                     current->right = insert_helper(current->right,new_node);
                 }
@@ -212,23 +243,33 @@ class RBTree
 
             //but wait wait...we are NOT done yet
             //finally, new node inserted at bottom and current is its parent
+
             //STEP 2: Validate RB Tree properties
 
             if(new_node->parent == current) //so validation occurs only once
             //balancing only required ONCE in insertion case, but NOT the same for deletion
             {
                 Balance(new_node,current);
+
+                //Now just CLIMB to the top from here and whatever is 
+                //the topmost thing..just make it the root
+
+                while(current->parent !=NULL) current = current->parent;
+                root = current; //SUPER SUPER IMPORTANT step
+                //because the tree is rotating around all the time...so root keeps changing
+                
+                if(current->left->right == NULL)cout<<endl<<"ooooooo";
+                return current;
             }
-            
-            return current;
         }
         //this function always returns the links to root
 
     public:
     RBNode * LRU_node; //keep updating this node as we go deleting LRU elements
-
+    RBNode * root; //make this private later..this is just for Inorder Traversal testing
     RBNode  * insert(int key, int val, int time_stamp)
     {
+        cout<<"check1"<<endl;
         RBNode * new_node = new RBNode(key, val, time_stamp);
         //note: default colour is red
 
@@ -241,14 +282,60 @@ class RBTree
         else
         {
             root = insert_helper(root,new_node);
+            //old root, new_node
         }
-
+        cout<< new_node->key << ":" << new_node->value <<" ("<< new_node->time_stamp <<") colour: "<<new_node->colour <<endl;    
         return new_node;
         //I will thus be returning the new_node's address so that it can be put into the 
         //hashmap also
         
         //hashmap shall have the key followed by node address
     }
-        
+
+    void Level_Order()
+    {
+        queue <RBNode*> q; //lets just store timestamps for now
+        //for testing
+
+        q.push(root);
+
+        while(!q.empty())
+        {
+            cout<<q.front()->key<<" :"<<q.front()->value<<" ("<<q.front()->time_stamp <<")"<<endl;
+            if(q.front()->left !=NULL)
+            {
+                q.push(q.front()->left);
+            }
+            else
+            {
+                q.push(q.front()->right);
+            }
+            q.pop();
+        }
+    }
+
+    void Inorder(RBNode * node)
+    {
+        if(node == NULL) return;
+        Inorder(node->left);
+        cout<<node->key<<"io:"<<node->value<<" ("<<node->time_stamp <<") colour: "<<node->colour<<endl;    
+        Inorder(node->right);
+    }
+
+    RBTree()
+    {
+        root = NULL;
+        LRU_node = NULL;
+    }
 };
 
+
+int main()
+{
+    RBTree T;
+    RBNode * n1 = T.insert(1,1,100);
+    RBNode * n2 =T.insert(2,2,101);
+    RBNode * n3 = T.insert(3,3,102);
+
+    // T.Inorder(T.root);
+}
